278b22b9b6277513131a9c7c369aa5161ffb4548
KurokoTakaakiGitHub <66106869+KurokoTakaakiGitHub@users.noreply.github.com>
2022-03-02 08:11:33 +0900

    create html

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,37 @@ import React from "react";
 import "./styles.css";
 
 const App = () => {
-  return <h1>こんにちは!</h1>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODO入力"></input>
+        <button>追加</button>
+      </div>
+      <div></div>
+      <ul>
+        <p>未完了のタスク</p>
+        <div>
+          <li>ああああ</li>
+          <button>完了</button>
+          <button>削除</button>
+        </div>
+        <div>
+          <li>いいいい</li>
+          <button>完了</button>
+          <button>削除</button>
+        </div>
+      </ul>
+      <div>
+        <ul>
+          <p>完了のタスク</p>
+          <div>
+            <li>うううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default App;
